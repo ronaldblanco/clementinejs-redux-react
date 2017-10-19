d427461d08afe576b1a8ef200ef2baabdab13487
Ronald Blanco <pnald.blanco@gmail.com>
2017-10-19 22:27:47 +0000

    Lint fix 2

--- client/src/components/localauth/ResetLocal.jsx
+++ client/src/components/localauth/ResetLocal.jsx
@@ -3,34 +3,54 @@ import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { getMess } from '../../reducer';
 import * as actionCreators from '../../actions';
-//console.log(actionCreators);
-/*const ResetLocal = React.createClass({
+// console.log(actionCreators);
+/* const ResetLocal = React.createClass({
 	render: function(message, resetlocal) {
-		return (*/
+		return ( */
 const ResetLocal = ({ message, resetlocal }) => (
-			<div className="container">
-				<div>
-					<div className={message.type} id="message"><h3>{ message.message }</h3></div>
-					
-					<img src="img/clementine_150.png" />
-					<br />
-					<p className="clementine-text">Clementine.js</p>
-					<Link className="menu" className="btn" id="login-btn" to={"/authlocal"}>Login Local User</Link>
-					<Link className="menu" className="btn" id="login-btn" to={"/createlocal"}>Create Local User</Link>
-					<Link className="menu" to={"/login"}>Return to Login Page</Link>
-				</div>
-				
-				<div>
-					<div className="alert alert-warning"><h5>Only valid if your username is a valid email!</h5></div><br/> 
-					<label>Username:</label><input type="text" name="name" id="resetusername" className="form-control" placeholder="Email" />
-					<br/>  
-					<button onClick={resetlocal} type="submit" className="btn btn-add btn-primary" id ="resetaction">Reset Password!</button>
-				</div>
-			</div>
+  <div className="container">
+    <div>
+      <div className={message.type} id="message"><h3>{message.message}</h3></div>
+      <img src="img/clementine_150.png" role="presentation" />
+      <br />
+      <p className="clementine-text">Clementine.js</p>
+      <Link className="menu" className="btn" id="login-btn" to={"/authlocal"}>
+        Login Local User
+      </Link>
+      <Link className="menu" className="btn" id="login-btn" to={"/createlocal"}>
+        Create Local User
+      </Link>
+      <Link className="menu" to={"/login"}>
+        Return to Login Page
+      </Link>
+    </div>
+    <div>
+      <div className="alert alert-warning">
+        <h5>Only valid if your username is a valid email!</h5>
+      </div><br />
+      <label>Username:</label>
+      <input
+        type="text"
+        name="name"
+        id="resetusername"
+        className="form-control"
+        placeholder="Email"
+      />
+      <br />
+      <button
+        onClick={resetlocal}
+        type="submit"
+        className="btn btn-add btn-primary"
+        id="resetaction"
+      >
+        Reset!
+      </button>
+    </div>
+  </div>
 		);
 /*		);
-	} 
-});*/
+	}
+}); */
 
 ResetLocal.propTypes = {
   message: React.PropTypes.object,
--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -9,18 +9,20 @@ export default () => (
       <br />
       <p className="clementine-text">Clementine-React-Redux</p>
       <a href="/auth/github">
-				<div className="btn" id="login-btn" width="32px" >
-							<img src="img/github_32px.png" alt="github logo" />
-							<p>LOGIN WITH GITHUB</p>
-						</div>
+        <div className="btn" id="login-btn" width="32px" >
+          <img src="img/github_32px.png" alt="github logo" />
+          <p>LOGIN WITH GITHUB</p>
+        </div>
       </a>
       <a href="auth/twitter">
         <div className="btn" id="login-btn" width="32px">
-						<img src="img/twitter_32new.png" alt="twitter logo"  />
-						<p>LOGIN WITH TWITTER</p>
-					</div>
+          <img src="img/twitter_32new.png" alt="twitter logo" />
+          <p>LOGIN WITH TWITTER</p>
+        </div>
       </a>
-      <Link className="btn" id="login-btn" to={"/authlocal"}><img src="img/local.png" alt="twitter logo" width="32px" />LOCAL LOGIN</Link>
+      <Link className="btn" id="login-btn" to={"/authlocal"}>
+        <img src="img/local.png" alt="twitter logo" width="32px" />LOCAL LOGIN
+      </Link>
     </div>
   </div>
 );
