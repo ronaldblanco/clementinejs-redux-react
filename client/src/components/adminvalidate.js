feaac96ade7bb362b11227fc21b6c1557484a378
Ronald Blanco <pnald.blanco@gmail.com>
2017-11-21 17:14:46 +0000

    lint final check

--- client/src/components/adminvalidate.js
+++ client/src/components/adminvalidate.js
@@ -30,7 +30,7 @@ export const validate = values => {
         userErrors.email = 'Required';
         usersArrayErrors[userIndex] = userErrors;
       }
-      if (user && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(user.email)){
+      if (user && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(user.email)) {
         userErrors.email = 'This is not a valid email!';
         usersArrayErrors[userIndex] = userErrors;
       }
