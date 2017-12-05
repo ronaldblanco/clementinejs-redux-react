4fcd1b0123e5cfb27a69d86accee948a13efa28c
Ronald Blanco <pnald.blanco@gmail.com>
2017-12-05 13:40:32 +0000

    organizing Socket and bluebird on datasHandler too

--- app/controllers/dataHandler.server.js
+++ app/controllers/dataHandler.server.js
@@ -4,38 +4,30 @@ import url from 'urlparser';
 /* eslint-disable func-names */
 /* eslint-disable consistent-return */
 function DataHandler() {
-  this.getDatas = (req, res, next) => {
-    Users
-      .findOne({ 'twitter.id': req.user.twitter.id }/* , { '_id': false } */)
-      .exec((err, result) => {
-        if (err) { return next(err); }
-        res.json(result.info);
-      }).catch(next);
+  this.getDatas = function* (req, res) {
+    const vars = yield Users
+      .findOne({ 'twitter.id': req.user.twitter.id })
+      .exec();
+      res.json(vars.info);
   };
 
-  this.addData = (req, res, next) => {
-    // const myUrl = url.parse(req.originalUrl);
+  this.addData = function* (req, res) {
     const name = req.originalUrl.split('?name=')[1].split('?value=')[0];
     const value = req.originalUrl.split('?value=')[1];
-    // console.log(myUrl);
     const newData = { name: unescape(name), value: unescape(value) };
-    Users
+    const vars = yield Users
       .findOneAndUpdate({ 'twitter.id': req.user.twitter.id }, { $push: { 'info.data': newData } })
-      .exec((err, result) => {
-        if (err) { return next(err); }
-        res.json(result.info);
-      }).catch(next);
+      .exec();
+      res.json(vars.info);
   };
 
-  this.deleteData = (req, res, next) => {
+  this.deleteData = function* (req, res) {
     const myUrl = url.parse(req.originalUrl);
-    Users
+    const vars = yield Users
       .findOneAndUpdate({ 'twitter.id': req.user.twitter.id },
       { $pull: { 'info.data': { name: unescape(myUrl.query.params.name) } } })
-      .exec((err, result) => {
-        if (err) { return next(err); }
-        res.json(result.info);
-      }).catch(next);
+      .exec();
+      res.json(vars.info);
   };
 
   this.getAllDatas = (req, res, next) => {
